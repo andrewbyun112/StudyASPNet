e4fd9621fe7f1c28de90ef71719d2c0f2e4221e1
seungjae <nimbus112@naver.com>
2020-08-19 09:27:07 +0900

    20200819_EddyHomePageSolution

---
+++ EddyHomePageSolution/UnitTest1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.AccessControl;
+using EddyNewHome.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CommonGetIp()
+        {
+            string expectedIp = "210.119.12.75";
+
+            string actualIp = Commons.GetIpAddress();
+            Assert.AreEqual(expectedIp, actualIp);
+
+        }
+    }
+}
